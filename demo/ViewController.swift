ad8b83f49da0f8710b153ca914b9c0f2fda96fef
MacStudent <macstudent@126-iMac15.local>
2019-03-05 17:42:38 -0500

    created calculator project_IOS_APP

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -4,17 +4,38 @@
 //
 //  Created by MacStudent on 2019-03-05.
 //  Copyright © 2019 MacStudent. All rights reserved.
-//
+ 
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var a:Int=0
+    var b:Int=0
+    var c:Int=0
+    @IBOutlet weak var txtno1: UITextField!
+    
+    @IBOutlet weak var lblresult: UILabel!
+    @IBOutlet weak var txtno2: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func btnadd(_ sender: UIButton) {
+        a=Int(txtno1.text!)!
+        b=Int(txtno2.text!)!
+        c=a+b
+        lblresult.text=String(c)
+    }
+    
+    
+    @IBAction func btnsubmit(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
